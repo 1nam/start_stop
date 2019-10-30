d041d7f9302f48888557b24c2c9c3ad07e929da8
zeus <33592162+1nam@users.noreply.github.com>
2019-10-30 03:31:24 -0400

    Create start_stop.py

---
+++ start_stop.py
@@ -0,0 +1,5 @@
+#simple type hello to start and bye to stop
+start_App = "hello"
+start_App = input("type hello to the start App: ")
+quit_App = "bye"
+quit_App = input("type bye to quit the App: ")
